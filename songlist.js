e54ea960b0107ffb339f0dd179a69c491cd46944
贺晓滨 <919760638@qq.com>
2025-01-14 15:20:57 +0800

    first commit

---
+++ songlist.js
@@ -0,0 +1,86 @@
+songlist = [
+    {fileName: "八嘎八嘎汉堡嘎-2"
+    },
+    {fileName: "迪迦主题曲日语版-59"
+    },
+    {fileName: "干物妹小埋-53"
+    },
+    {fileName: "君の知らない物语(较全)-48"
+    },
+    {fileName: "LOSER-47"
+    },
+    {fileName: "想你想你(未确定)-46"
+    },
+    {fileName: "アイドル-43"
+    },
+    {fileName: "爱你(王心凌)-38"
+    },
+    {fileName: "アイドル(糊弄版)-38"
+    },
+    {fileName: "爱杀宝贝-34"
+    },
+    {fileName: "催眠术-32"
+    },
+    {fileName: "病名为爱-31"
+    },
+    {fileName: "迪迦主题曲-29"
+    },
+    {fileName: "梅菲斯特-26"
+    },
+    {fileName: "后来日语版-25"
+    },
+    {fileName: "梅菲斯特辩解-24"
+    },
+    {fileName: "神龙斗士-18"
+    },
+    {fileName: "giligili爱-18"
+    },
+    {fileName: "残酷天使的行动纲领-17"
+    },
+    {fileName: "残酷天使的行动纲领(夹&不夹)-17"
+    },
+    {fileName: "东京食尸鬼-17"
+    },
+    {fileName: "这首不知道-17"
+    },
+    {fileName: "君の知らない物语(高潮)-16"
+    },
+    {fileName: "一般脱粪男子-16"
+    },
+    {fileName: "某儿歌-15"
+    },
+    {fileName: "卡哇伊-15"
+    },
+    {fileName: "欺负群友不懂日语-13"
+    },
+    {fileName: "girigili爱(微瑕)-13"
+    },
+    {fileName: "欺负群友不懂日语2-12"
+    },
+    {fileName: "欺负群友不懂日语3-11"
+    },
+    {fileName: "水浒传-10"
+    },
+    {fileName: "暴言暴语-9"
+    },
+    {fileName: "残酷天使纲领忘词-7"
+    },
+    {fileName: "上海宁-7"
+    },
+    {fileName: "被瓢虫吓到-7"
+    },
+    {fileName: "欺负群友不懂日语4-6"
+    },
+    {fileName: "问到点子上了-6"
+    },
+    {fileName: "日语课-6"
+    },
+    {fileName: "你谁-5"
+    },
+    {fileName: "被瓢虫憋住恋爱循环-5"
+    },
+    {fileName: "喜羊羊-4"
+    },
+    {fileName: "甘い-3"
+    }
+]
